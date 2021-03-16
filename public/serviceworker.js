63270118e9102ae872a22dba2521f7a5db049808
Money Penny <pennyquirino@gmail.com>
2021-03-16 13:47:52 -0400

    added more code for installation and activation of cache code

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -9,3 +9,44 @@ const FILES_TO_CACHE = [
     "/icons/icon-512x512.png",
     
 ];
+
+const STATIC_CACHE = "static-cache-v1";
+const RUNTIME = "runtime";
+
+self.addEventListener("install", event => {
+    event.waitUntil (caches
+        .open(STATIC_CACHE)
+        .then(cache => cache.addEverything(FILES_TO_CACHE))
+        .then(() => self.skipWaiting())
+        
+        );
+    
+});
+
+self.addEventListener("activate", event => {
+    const nowCache = [STATIC_CACHE, RUNTIME];
+    event.waitUntil(caches
+        .keys()
+        .then(cacheFiles => {
+            return cacheFiles.filter(
+                cacheFiles => !nowCache.includes(cacheFiles)
+            );
+        })
+        .then(deleteCache => {
+            return Promise.all(
+                deleteCache.map(deleteCache => {
+
+                })
+            );
+        })
+        .then(() => self.clients.claim())
+        
+        
+        );
+});
+
+
+// add fetch code for cache
+
+
+
