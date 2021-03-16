06c9dc41c22e1e643b0e5e668241a656ac1084ad
Money Penny <pennyquirino@gmail.com>
2021-03-16 13:23:36 -0400

    created a database file and starting code

---
+++ public/database.js
@@ -0,0 +1,27 @@
+let db;
+
+const request = indexedDB.open("budget", 1);
+
+request.onupgradeneeded = function (event) {
+    const db = event.target.result;
+    db.createObjectStore("pending", { autoIncrement: true });
+};
+
+request.onsuccess = function (event) {
+    db = event.target.result;
+
+    if (navigator.onLine) {
+        checkDb();
+    }
+};
+
+request.onerror = function (event) {
+    console.log("Error: " + event.target.errorCode);
+};
+
+function saveRecord(record) {
+    const transaction = db.transaction(["pending"], "readwrite");
+    const store = transaction.objectStore("pending");
+
+    store.add(record);
+}
